ce552c630a4f3539ff450ba0a275c2cfbd9ae9e7
RobertDrone <robertdrone@ovi.com>
2014-09-19 23:47:18 -0600

    Revert "pulling changes from aospa-legacy"

--- src/java/android/telephony/MSimSmsManager.java
+++ src/java/android/telephony/MSimSmsManager.java
@@ -550,7 +550,7 @@ public class MSimSmsManager {
             // ignore it
         }
 
-        return createMessageListFromRawRecords(records, subscription);
+        return createMessageListFromRawRecords(records);
     }
 
     /**
@@ -702,8 +702,7 @@ public class MSimSmsManager {
      *   <code>getAllMessagesFromIcc</code>
      * @return <code>ArrayList</code> of <code>SmsMessage</code> objects.
      */
-    private static ArrayList<SmsMessage> createMessageListFromRawRecords(List<SmsRawData> records,
-            int subscription) {
+    private static ArrayList<SmsMessage> createMessageListFromRawRecords(List<SmsRawData> records) {
         ArrayList<SmsMessage> messages = new ArrayList<SmsMessage>();
         if (records != null) {
             int count = records.size();
@@ -711,8 +710,7 @@ public class MSimSmsManager {
                 SmsRawData data = records.get(i);
                 // List contains all records, including "free" records (null)
                 if (data != null) {
-                    SmsMessage sms = SmsMessage.createFromEfRecord(i+1, data.getBytes(),
-                            subscription);
+                    SmsMessage sms = SmsMessage.createFromEfRecord(i+1, data.getBytes());
                     if (sms != null) {
                         messages.add(sms);
                     }
@@ -804,26 +802,6 @@ public class MSimSmsManager {
             return false;
         }
     }
-
-    /**
-     * Get the capacity count of sms on Icc card
-     *
-     * @return the capacity count of sms on Icc card
-     * @hide
-     */
-    public static int getSmsCapacityOnIcc(int subscription) {
-        int ret = -1;
-        try {
-            ISmsMSim iccISms = ISmsMSim.Stub.asInterface(ServiceManager.getService("isms_msim"));
-            if (iccISms != null) {
-                ret = iccISms.getSmsCapacityOnIcc(subscription);
-            }
-        } catch (RemoteException ex) {
-            //ignore it
-        }
-        return ret;
-    }
-
     // see SmsMessage.getStatusOnIcc
 
     /** Free space (TS 51.011 10.5.3 / 3GPP2 C.S0023 3.4.27). */
